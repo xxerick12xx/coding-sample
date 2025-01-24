3d3338dbc9551c0ab39316c20cd1c8822a51c686
xxerick12xx <erickmacha12@gmail.com>
2025-01-25 01:57:55 +0800

    add navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { TablePage } from "./pages/";
+import { Header } from "./components";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<TablePage />} />
       </Routes>
---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { NavLink } from "react-router";
+import { Button } from "./../../ui";
+
+const NavItem = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => (
+  <Button variant="ghost">
+    <NavLink to={href}>{children}</NavLink>
+  </Button>
+);
+
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-background">
+      <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <NavLink to="/" className="text-xl font-bold">
+                Logo
+              </NavLink>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <NavItem href="/create">Create Referral</NavItem>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <Button
+              onClick={() => setIsOpen(!isOpen)}
+              variant="primary"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <X className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <Menu className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden bg-background" id="mobile-menu">
+          <div className="pt-4 pb-3 border-t border-blue-700 flex flex-col">
+            <NavItem href="/create">Create Referral</NavItem>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
